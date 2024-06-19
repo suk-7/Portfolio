09ddbfc9f31d483a9bcd029e02deeb87c63cbb06
SukanyaRani <srani6@asu.edu>
2024-06-18 21:34:00 -0700

    Added content for skills part

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const header = document.querySelector("header");
 
+const first_skill=document.querySelector(".skill:first-child");
+const sk_counters=document.querySelectorAll(".counter span");
+const progress_bars=document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll", () =>{
+    skillsCounter();
+})
 /*--------------Sticky Navbar------------*/
 
 function stickyNavbar(){
@@ -17,3 +24,14 @@ let sr=ScrollReveal({
 
 sr.reveal(".showcase-info",{delay:300});
 sr.reveal(".showcase-image",{origin: "top",delay:400});
+
+/*--------------Skills Progress Bar Animation------------*/
+
+function hasReached(el){
+    let topPosition=el.getBoundingClientRect().top;
+    console.log(topPosition);
+}
+function skillsCounter(){
+    console.log("Here I am");
+    hasReached(first_skill);
+}
